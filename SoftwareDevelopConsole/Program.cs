23294830e026d5e4f75c367e8035b6bf92590791
zart227 <zart227@live.ru>
2020-11-09 11:21:09 +0300

    начало разработки

---
+++ SoftwareDevelopConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareDevelopConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
